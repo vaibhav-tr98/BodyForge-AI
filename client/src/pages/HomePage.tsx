4627eda08dfb61ff55062d41e74441e4908775d3
vaibhav-tr98 <vaibhavtripathi99a@gmail.com>
2026-07-12 00:52:14 +0530

    feat: setup frontend architecture with routing and Tailwind CSS

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-red-900 text-white">
+      <h1 className="text-6xl">HOME PAGE</h1>
+    </div>
+  );
+}
+
+export default HomePage;
